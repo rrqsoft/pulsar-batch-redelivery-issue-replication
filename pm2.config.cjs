c69c3e1bef896190182966896d36852e56800246
Ael Festijo <ray.quijada@snsofttech.ph>
2026-04-06 09:58:57 +0800

    chore: moved back default multiple instances

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -18,23 +18,23 @@ module.exports = {
         BATCH_INDEX_ACK_ENABLED: 'false'
       }
     },
-    // {
-    //   name: 'pulsar-worker-2',
-    //   script: './worker.js',
-    //   instances: 1,
-    //   autorestart: true,
-    //   watch: false,
-    //   max_memory_restart: '500M',
-    //   env: {
-    //     WORKER_ID: '2',
-    //     PULSAR_URL: 'pulsar://localhost:6650',
-    //     TOPIC: 'persistent://public/default/test-batch-redelivery',
-    //     SUBSCRIPTION: 'test-batch-sub',
-    //     BATCH_SIZE: '10',
-    //     BATCH_TIMEOUT_MS: '10000',
-    //     ACK_TIMEOUT_MS: '15000',
-    //     BATCH_INDEX_ACK_ENABLED: 'false'
-    //   }
-    // }
+    {
+      name: 'pulsar-worker-2',
+      script: './worker.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        WORKER_ID: '2',
+        PULSAR_URL: 'pulsar://localhost:6650',
+        TOPIC: 'persistent://public/default/test-batch-redelivery',
+        SUBSCRIPTION: 'test-batch-sub',
+        BATCH_SIZE: '10',
+        BATCH_TIMEOUT_MS: '10000',
+        ACK_TIMEOUT_MS: '15000',
+        BATCH_INDEX_ACK_ENABLED: 'false'
+      }
+    }
   ]
 };
